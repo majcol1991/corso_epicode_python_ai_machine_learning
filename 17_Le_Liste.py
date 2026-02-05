f0911d7c15ab966ce28afc24486159e4846f9892
majcol <majcol.webdev@gmail.com>
2026-02-05 17:05:08 +0100

    Esercizi liste

---
+++ 17_Le_Liste.py
@@ -0,0 +1,18 @@
+# Esercizio 1
+
+numeri = [0, 1, 2]
+numeri.append(5)
+print(numeri)
+
+
+# Esercizio 2
+
+frutti = ["mela","pera"]
+frutti.insert(3,"orange")
+print(frutti)
+
+# Esercizio 3
+
+numeri = [0, 1, 2]
+numeri.remove(2)
+print(numeri)
