5ec2d934be5a5248a492d315656d44659b62af54
majcol <majcol.webdev@gmail.com>
2026-02-05 16:09:51 +0100

    Esercizi Booleani

---
+++ 9_Boolean.py
@@ -0,0 +1,20 @@
+# Esercizio 1 
+
+x = 10 
+risultato = x > 0 
+print(risultato)
+
+
+# Esercizio 2
+
+s1 = "ciao mondo"
+s1 = "ciao mondo"
+print ( s1 == s2 ) 
+
+# Esercizio 3 
+
+a = 5
+b = 10 
+
+risultato = (a > 0) and (b > 0)
+print(risulato)
