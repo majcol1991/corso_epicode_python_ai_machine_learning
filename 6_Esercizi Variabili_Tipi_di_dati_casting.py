dc2b87c3327868df3fffdb937c3b03148dbaaabc
majcol <majcol.webdev@gmail.com>
2026-02-05 11:04:27 +0100

    Esercizi Variabili, tipi di dati e Casting

---
+++ 6_Esercizi Variabili_Tipi_di_dati_casting.py
@@ -0,0 +1,104 @@
+# Esercizio 1 
+
+nome = "Francesco"
+età = 40
+città = "Milano"
+
+print(nome, età, città)
+
+
+# Esercizio 2
+
+x = 5
+print(x)
+x = 10
+print(x) 
+
+
+# Esercizio 3
+
+x = 10
+y = 20
+print("Somma:", x + y)  
+
+
+# Esercizio 4
+
+x = 20
+y = 15
+
+x, y = y, x
+print(x, y)  
+
+
+# Esercizio 5 
+
+base = 5
+altezza = 10
+area = base * altezza
+print("Area rettangolo:", area)  
+
+
+# Esercizio 6 
+
+a = 10
+b = 3.5
+print(a + b)  
+
+
+# Esercizio 7
+
+x, y, z = 10, 40, 30
+media = (x + y + z) / 3
+print("Media:", media)  
+
+
+# Esercizio 8 
+
+s1 = "Mi piace "
+s2 = " giocare a calcio"
+print(s1 + s2)  
+
+
+# Esercizio 9 
+
+print("Ciao " * 3)  
+
+
+# Esercizio 10 
+
+a = 15
+b = 30
+c = 45 
+print(a < b)
+print(b > c) 
+print(a < c)  
+
+
+# Esercizio 11 
+
+x = 10
+y = float(x)
+print(y)  
+
+
+# Esercizio 12
+
+n = "4550"
+s = int(n)
+
+
+
+# Esercizio 13
+
+print (bool(10))
+print (bool(0))
+print (bool(-100))
+print (bool(""))
+
+
+
+
+
+
+ 
