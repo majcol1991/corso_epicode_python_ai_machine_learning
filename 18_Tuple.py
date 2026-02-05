b8616872f69beedfb49dcdca8349781813eedede
majcol <majcol.webdev@gmail.com>
2026-02-05 17:13:29 +0100

    Esercizi tuple

---
+++ 18_Tuple.py
@@ -0,0 +1,17 @@
+# Esercizio 1
+
+giorni = ( "lun", "mar", "mer","gio")
+for i in giorni:
+    print (f"{i}")
+
+
+# Esercizio 2
+
+x = (5,)
+print(type(x))
+
+
+# Esercizio 3
+
+colori = ("rosso", "blu", "rosso", "giallo")
+print(colori.count("rosso"))  # 2
